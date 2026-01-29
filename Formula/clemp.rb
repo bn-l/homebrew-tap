4b8a2416464f73cb387e5468d9d3bcea9bc1ca25
bn-l <13162197+bn-l@users.noreply.github.com>
2026-01-29 17:33:02 +1100

    clemp 0.1.5

--- Formula/clemp.rb
+++ Formula/clemp.rb
@@ -3,8 +3,8 @@
 class Clemp < Formula
   desc "CLI tool to clone and configure claude-template for your project"
   homepage "https://github.com/bn-l/clemp"
-  url "https://github.com/bn-l/clemp/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "669b861543ea3dcdfe0b1b74c9c71a7ba57f1a2ddd9367aa285f26c6db1776cf"
+  url "https://github.com/bn-l/clemp/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "8027e24e646e9f4fc9388a55754b6c2e477e80778bd162dfbbb5778259d8e3e8"
   license "MIT"
 
   head "https://github.com/bn-l/clemp.git", branch: "master"
