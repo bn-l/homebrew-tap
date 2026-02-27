41f2f46aab32f0f171009620658c50b2c4e98b6c
bn-l <13162197+bn-l@users.noreply.github.com>
2026-02-27 17:21:08 +1100

    clacal 1.5.2

--- Casks/clacal.rb
+++ Casks/clacal.rb
@@ -1,6 +1,6 @@
 cask "clacal" do
-  version "1.5.1"
-  sha256 "de5c06dadc28ae89322d6057370249703fed666fd5645fbf9dd401eef5df4440"
+  version "1.5.2"
+  sha256 "f90594abbeb4a9a8ad4a556bba9b6825e9e95408b3f7f449bc923d4209cbac21"
   url "https://github.com/bn-l/clacal/releases/download/v#{version}/Clacal_#{version}.dmg"
 
   name "Clackal"
