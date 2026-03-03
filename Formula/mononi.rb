cb40ba17b1b4440ec08653fe1208855205e70147
bn-l <13162197+bn-l@users.noreply.github.com>
2026-03-03 21:51:17 +1100

    mononi 0.2.0

--- Formula/mononi.rb
+++ Formula/mononi.rb
@@ -3,8 +3,8 @@
 class Mononi < Formula
   desc "Theme scheduler for macOS — morning, noon, and night"
   homepage "https://github.com/bn-l/mononi"
-  url "https://github.com/bn-l/mononi/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fcf003d61e350bf3ea9c95532f33d1da6e95664553cc546871c64e2524613fd2"
+  url "https://github.com/bn-l/mononi/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "22f50cd449e03262b5695aaa31bb355bb1236c9393af3bc112d90ff4cd54a2b5"
   license "MIT"
 
   head "https://github.com/bn-l/mononi.git", branch: "main"
