d28167a739bd58c6c65e0f85970d985214fe6295
bn-l <13162197+bn-l@users.noreply.github.com>
2026-03-17 12:08:28 +1100

    dynio 1.3.0

--- Casks/dynio.rb
+++ Casks/dynio.rb
@@ -1,13 +1,13 @@
 cask "dynio" do
-  version "1.2.2"
+  version "1.3.0"
 
   on_arm do
-    sha256 "e58fba7041effd9cd24aa7933aff5f054dfadb0c99024f9bc91b792fe3e14649"
+    sha256 "cd7809b0fb9ecd70180b02eaca6e377841e7bf5297c12b7822694553f8d9362e"
     url "https://github.com/bn-l/dynio/releases/download/dynio-v#{version}/dynio_#{version}_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "6a4f9f7f9fddae4d2396c114b0270735ef396ccc2ab0e53e836fe06f8e8f335d"
+    sha256 "e2d2ba2f9a9e0c38a63cb0857facf8e648d41607192cd5717e3b72be66f24a66"
     url "https://github.com/bn-l/dynio/releases/download/dynio-v#{version}/dynio_#{version}_x64.dmg"
   end
 
