139e118863b3a068146a054a5bed907ea84a9552
bn-l <13162197+bn-l@users.noreply.github.com>
2026-04-08 21:26:42 +1000

    easy-mac-zip 0.2.0

--- Formula/easy-mac-zip.rb
+++ Formula/easy-mac-zip.rb
@@ -3,8 +3,8 @@
 class EasyMacZip < Formula
   desc "Friendly wrapper around 7zz for easy zipping and unzipping on macOS"
   homepage "https://github.com/bn-l/easy-mac-zip"
-  url "https://github.com/bn-l/easy-mac-zip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6098d3d6dea470f246f9fcdbff32a8a65ad88f98891c8394d6d4ffccc30a9f64"
+  url "https://github.com/bn-l/easy-mac-zip/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "222c22f935e2f5cf88caa0138a7c411d211644d29a656b9f4498d364b3f57482"
   license "MIT"
 
   head "https://github.com/bn-l/easy-mac-zip.git", branch: "main"
