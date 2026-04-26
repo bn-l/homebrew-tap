2b2ef5cf1151a2e37cd02805ce3bfbcaa7b19b1a
bn-l <13162197+bn-l@users.noreply.github.com>
2026-04-26 13:25:21 +1000

    clarg 0.3.3

--- Formula/clarg.rb
+++ Formula/clarg.rb
@@ -3,8 +3,8 @@
 class Clarg < Formula
   desc "Claude Code hook handler for blocking commands and file access"
   homepage "https://github.com/bn-l/clarg"
-  url "https://github.com/bn-l/clarg/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "6a631c3f51a80549bc5a4bd82ca1017d32d010c9c39eecc86e014848ba0a46a6"
+  url "https://github.com/bn-l/clarg/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "3c6c7365b3ff96715091b73394c5dfc63559b6498261fe8c22c2301294eb04f4"
   license "MIT"
 
   head "https://github.com/bn-l/clarg.git", branch: "master"
