39c81843c251262d0a5821065ef81112a4bb1fcc
bn-l <13162197+bn-l@users.noreply.github.com>
2026-04-26 17:40:29 +1000

    cocal 1.9.0

--- Casks/cocal.rb
+++ Casks/cocal.rb
@@ -1,18 +1,18 @@
 cask "cocal" do
   version "1.9.0"
-  sha256 "ef10fe0faa013f8e414bbb96a4508026c7b7505e0f03907944ebe1aef6f1f6ce"
-  url "https://github.com/bn-l/cocal/releases/download/v#{version}/CodexSwitcher_#{version}.dmg"
+  sha256 "2f311508234b3ead3a1f477bae69e36ecb3e43e7ad2bd9237c9a3b788806647f"
+  url "https://github.com/bn-l/cocal/releases/download/v#{version}/Cocal_#{version}.dmg"
 
-  name "CodexSwitcher"
+  name "Cocal"
   desc "Claude code usage calibrator"
   homepage "https://github.com/bn-l/cocal"
 
   depends_on macos: ">= :sequoia"
 
-  app "CodexSwitcher.app"
+  app "Cocal.app"
 
   postflight do
     system_command "/usr/bin/xattr",
-                   args: ["-d", "com.apple.quarantine", "#{appdir}/CodexSwitcher.app"]
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/Cocal.app"]
   end
 end
