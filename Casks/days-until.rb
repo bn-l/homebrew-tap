aaacab6cd052d302485de8fccb3d7499ac73edc1
bn-l <13162197+bn-l@users.noreply.github.com>
2026-06-10 10:59:31 +1000

    days-until 0.1.0

---
+++ Casks/days-until.rb
@@ -0,0 +1,18 @@
+cask "days-until" do
+  version "0.1.0"
+  sha256 "d95c55459a96cb7250a853a2c5c9e0c9f2ffbc2b68b30511a080935faa6131c9"
+  url "https://github.com/bn-l/DaysUntil/releases/download/v#{version}/DaysUntil_#{version}.dmg"
+
+  name "DaysUntil"
+  desc "Menubar countdown app showing days remaining as badge icons"
+  homepage "https://github.com/bn-l/DaysUntil"
+
+  depends_on macos: ">= :sequoia"
+
+  app "DaysUntil.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/DaysUntil.app"]
+  end
+end
