01f736dc0ec90cadc7cc3280794b1366bfadbef7
bn-l <13162197+bn-l@users.noreply.github.com>
2026-06-11 11:52:44 +1000

    days-until 0.2.0

--- Casks/days-until.rb
+++ Casks/days-until.rb
@@ -1,6 +1,6 @@
 cask "days-until" do
-  version "0.1.0"
-  sha256 "d95c55459a96cb7250a853a2c5c9e0c9f2ffbc2b68b30511a080935faa6131c9"
+  version "0.2.0"
+  sha256 "78b7672309191653f57605dba4b468b9a336d57b7984d80227b8d3c1f8f41f6d"
 
   url "https://github.com/bn-l/DaysUntil/releases/download/v#{version}/DaysUntil_#{version}.dmg"
   name "DaysUntil"
