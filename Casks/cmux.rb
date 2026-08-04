3a1214cc5c291ce97f9466074779f5ffadc2a842
bn-l <13162197+bn-l@users.noreply.github.com>
2026-08-04 10:22:51 +1000

    cmux 1.45.2

--- Casks/cmux.rb
+++ Casks/cmux.rb
@@ -1,6 +1,6 @@
 cask "cmux" do
-  version "1.45.1"
-  sha256 "c4285a7bd2f1efc0f327b4945e4d39b61a05706a16dc96c95b4538819404c1bd"
+  version "1.45.2"
+  sha256 "053deedf9f185809423ce1c1793054b5af8ffeaa465371240424c5a854f08a5c"
   url "https://github.com/bn-l/cmux/releases/download/v#{version}/cmux-macos.dmg"
 
   name "cmux"
